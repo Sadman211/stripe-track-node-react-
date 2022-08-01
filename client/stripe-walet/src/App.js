bb611eebb1356c66104850c294a8edafc545fb29
sadman211 <sadman@6sensehq.com>
2022-08-01 12:11:20 +0600

    add apple and google pay components

--- client/stripe-walet/src/App.js
+++ client/stripe-walet/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import CheckoutForm from './CheckoutForm';
+import ApplePay from './ApplePay';
+import GooglePay from './GooglePay';
+import List from './List';
 
 // Make sure to call `loadStripe` outside of a component's render to avoid
 // recreating the `Stripe` object on every render.
@@ -11,7 +14,17 @@ const stripePromise = loadStripe('pk_test_51LRXLaCODFLmmIa5usY3Rdy22Z6tK8nRdXaLp
 function App() {
   return (
     <Elements stripe={stripePromise}>
-      <CheckoutForm />
+      {/* <CheckoutForm /> */}
+      <>
+      <a href="/">home</a>
+      <BrowserRouter>
+        <Routes>
+         <Route path="/" element={<List />} />
+          <Route path="/apple-pay" element={<ApplePay />} />
+          <Route path="/google-pay" element={<GooglePay />} />
+        </Routes>
+      </BrowserRouter>
+    </>
     </Elements>
   );
 };
